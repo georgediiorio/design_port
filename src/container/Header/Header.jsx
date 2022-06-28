2a53c02b670c8f48c623af1fce0222ba99f64527
George Di Iorio <62784063+georgediiorio@users.noreply.github.com>
2022-06-27 19:07:30 -0700

    Initial Commit.

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+import { images } from '../../constants'
+import './Header.scss'
+
+import './Header.scss'
+const Header = () => {
+  return (
+    <div className='app__header app__flex'>
+      
+    </div>
+  )
+}
+
+export default Header
